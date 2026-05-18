65b152fdfdcdcbb015d497d72911f632cabfd6d0
yh8922 <msgb@sh.re.kr>
2026-05-18 02:32:55 +0000

    EVEN_ODD

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# 웹 페이지 제목 설정
+st.title("짝수/홀수 판별기")
+
+# 기존 코드: n = int(input())
+# 스트림릿의 number_input을 사용해 자연수를 입력받습니다.
+n = st.number_input("자연수 정숫값을 입력하세요:", min_value=1, step=1, value=1)
+
+# 문제 조건에 맞추어 "even"과 "odd"로 출력하도록 유지 및 수정
+if n % 2 == 0:
+    # 기존 코드: print('짝수')
+    st.write("even")
+else:
+    # 기존 코드: print('홀수')
+    st.write("odd")
